76fa83972987fc431c9c24a2b962e897ed7596e7
alibabakutty <tallysriram007@gmail.com>
2025-12-08 19:03:17 +0530

    now send frotend for host

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -14,7 +14,6 @@ const MainPage = () => {
   const handleCorporateLogin = () => {
     navigate('/corporate-login');
   };
-
   console.log('Current API URL:', import.meta.env.VITE_API_URL);
 
   return (
--- src/components/right-side-button/LeftSideMenu.jsx
+++ src/components/right-side-button/LeftSideMenu.jsx
@@ -4,5 +4,4 @@ const LeftSideMenu = () => {
     <div className='bg-[#493D9E] w-[60%] h-[92.9vh]'></div>
   )
 }
-
 export default LeftSideMenu
